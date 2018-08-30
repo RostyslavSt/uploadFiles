9bbc4d64427d161df5ab50208b484ef148154e7b
Shtets <rostyslav.shtets@sigmaukraine.com>
2018-08-30 14:36:38 +0300

    refactoring.func for creating elements is created

--- js/main.js
+++ js/main.js
@@ -17,8 +17,7 @@ function handleFileSelect(e) {
     if(!e.target.files || !window.FileReader) return;
     let filesArr = [...e.target.files];
     console.dir(e.target.files);
-   
-    
+       
     filesArr.forEach(item => {
         let fileItem = item;
         let li = document.createElement('li');
@@ -51,27 +50,69 @@ function handleFileSelect(e) {
     })
 }
 
+//drag and drop
 let dropArea = document.getElementById('drop-area');
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults, false)
-  })
+    dropArea.addEventListener(eventName, preventDefaults);
+  });
+
 function preventDefaults (e) {
-    e.preventDefault()
-    e.stopPropagation()
+    e.preventDefault();
+    e.stopPropagation();
 }
 
-dropArea.addEventListener('drop', handleDrop, false)
-function handleDrop(e) {
-  let dt = e.dataTransfer
-  let files = dt.files
-  handleFiles(files)
-}
+dropArea.addEventListener('drop', function(e) {
+    let dt = e.dataTransfer;
+    let files = dt.files;
+    createElementList(files);
+    // handleFiles(files);
+});
+// function handleDrop(e) {
+//   let dt = e.dataTransfer;
+//   let files = dt.files;
+//   handleFiles(files);
+// }
 
-function handleFiles(files) {
-    console.log("drop");
-    console.log([...files]);
-    let filesArr = [...files];
+// function handleFiles(files) {
+//     console.log("drop");
+//     console.log([...files]);
+//     let filesArr = [...files];
 
+//     filesArr.forEach(item => {
+//         let fileItem = item;
+//         let li = document.createElement('li');
+//         let reader = new FileReader();
+//         reader.onload = function (e) {
+//             let fileNameExtension = fileItem.name.split('.')[1];
+//             let fileImgUrl = e.target.result
+
+//             switch(fileNameExtension) {
+//                 case 'pdf': fileImgUrl = "./img/pdf.png";
+//                     break;
+//                 case 'docx' : fileImgUrl = "./img/word.png";
+//                     break;
+//                 case 'doc' : fileImgUrl = "./img/word.png";
+//                     break;
+//                 case 'xlsx' : fileImgUrl = "./img/excel.png";
+//                     break;
+//                 case 'xls' : fileImgUrl = "./img/excel.png";
+//                     break;
+//                 case 'txt': fileImgUrl = "./img/text.png";
+//                     break;
+//             }
+
+//             let html = `<img src="${fileImgUrl}"> <span>${fileItem.name}</span>`;
+//             // let html = imgTemplate(fileImgUrl, fileItem.name);
+//             li.innerHTML += html;
+//             ul.appendChild(li);
+// 		}
+//         reader.readAsDataURL(fileItem); 
+//     })
+// }
+
+//function for creating list of files
+function createElementList(objWithFiles) {
+    let filesArr = [...objWithFiles];
     filesArr.forEach(item => {
         let fileItem = item;
         let li = document.createElement('li');
@@ -101,11 +142,7 @@ function handleFiles(files) {
             ul.appendChild(li);
 		}
         reader.readAsDataURL(fileItem); 
-    })
-
-
-
-
+    });
 }
 
 
