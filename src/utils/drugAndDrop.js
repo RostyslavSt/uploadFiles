e1054d4b05c86f75c743664232d38b7a4cfcb3e6
Shtets <rostyslav.shtets@sigmaukraine.com>
2018-09-05 15:19:00 +0300

    drag and drop refactoring

--- src/utils/drugAndDrop.js
+++ src/utils/drugAndDrop.js
@@ -2,8 +2,7 @@ import createElementList from './createElementList';
 
 //drag and drop
 function implementDrugAndDrop(ul, funcForGetFiles) {
-    // let filesFromDrugAndDrop = {};
-
+ 
     let dropArea = document.getElementById('drop-area');
     ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
         dropArea.addEventListener(eventName, preventDefaults);
@@ -13,19 +12,12 @@ function implementDrugAndDrop(ul, funcForGetFiles) {
         e.preventDefault();
         e.stopPropagation();
     }
-    // function func(filesFrom) {
-    //     filesFromDrugAndDrop = filesFrom;
-    // }
-
     dropArea.addEventListener('drop',  e => {
         let dt = e.dataTransfer;
         let filesFromDrugAndDrop = dt.files;
-        // console.log(filesFromDrugAndDrop);
         funcForGetFiles(dt.files);
         createElementList(filesFromDrugAndDrop, ul);
     });
-
-    // console.log(filesFromDrugAndDrop);
 }
 
 export default implementDrugAndDrop;
