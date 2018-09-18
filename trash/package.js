eeeaec9e50d80c20a15ee53201f3df905a53e8b3
Shtets <rostyslav.shtets@sigmaukraine.com>
2018-09-18 14:11:18 +0300

    new webpack config

---
+++ trash/package.js
@@ -0,0 +1,32 @@
+// {
+//     "name": "project-files-uploader",
+//     "version": "1.0.13",
+//     "description": "file uploader - test version",
+//     "main": "./dist/js/main.js",
+//     "dependencies": {
+//       "bootstrap": "^4.1.3",
+//       "npm": "^6.4.1",
+//       "uniqid": "^5.0.3",
+//       "webpack": "^4.17.1"
+//     },
+//     "devDependencies": {
+//       "babel-cli": "^6.26.0",
+//       "babel-core": "^6.26.3",
+//       "babel-preset-es2015": "^6.24.1",
+//       "css-loader": "^1.0.0",
+//       "jshint": "^2.9.6",
+//       "jshint-loader": "^0.8.4",
+//       "style-loader": "^0.23.0",
+//       "webpack-cli": "^3.1.0"
+//     },
+//     "scripts": {
+//       "test": "echo \"Error: no test specified\" && exit 1",
+//       "build": "webpack -p",
+//       "watch": "webpack --watch",
+//       "compile": "babel --presets es2015 -d lib/ src/",
+//       "prepublish": "npm run compile"
+//     },
+//     "author": "rost",
+//     "license": "ISC"
+//   }
+  
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,90 @@
-let path = require('path');
+// npm i webpack webpack-cli @babel/core @babel/plugin-proposal-object-rest-spread @babel/preset-env babel-loader -D
 
-module.exports = {
-  entry: './src/js/main.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: "commonjs2"
-  },
-  module: {
-    // loaders: [{
-    //   test: /\.(svg|ttf|woff|woff2|eot)$/,
-    //   loader: 'url?limit=5000'
-    // }, ],
+const path = require("path");
+const webpack = require("webpack");
+const webpack_rules = [];
+const webpackOption = {
+    entry: "./src/js/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        libraryTarget: "commonjs2"
 
-    rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: [/node_modules/],
-      //   use: [{
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: ['es2015']
-      //     }
-      //   }]
-      // },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      // {
-      //   test: /\.(svg|ttf|woff|woff2|eot)$/,
-      //   loader: 'url?limit=5000'
-      // },
-    ]
-  },
-
-//   plugins: [
-//     new webpack.ProvidePlugin({
-//         $: 'jquery',
-//         jQuery: 'jquery'
-//     })
-// ]
-  
-  watch: true
+    },
+    module: {
+        rules: webpack_rules
+    },
+    optimization: {
+      minimize: false
+    },
+    watch: true
 };
+let babelLoader = {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+        loader: "babel-loader",
+        options: {
+            presets: ["@babel/preset-env"]
+        }
+    }
+};
+
+let cssLoader = { 
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        }
+webpack_rules.push(babelLoader, cssLoader);
+module.exports = webpackOption;
+
+
+// -------------------------
+// let path = require('path');
+
+// module.exports = {
+//   entry: './src/js/main.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     libraryTarget: "commonjs2"
+//   },
+//   module: {
+//     // loaders: [{
+//     //   test: /\.(svg|ttf|woff|woff2|eot)$/,
+//     //   loader: 'url?limit=5000'
+//     // }, ],
+
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: "babel-loader"
+//         }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader'
+//         ]
+//       },
+//       // {
+//       //   test: /\.(svg|ttf|woff|woff2|eot)$/,
+//       //   loader: 'url?limit=5000'
+//       // },
+//     ]
+//   },
+
+// //   plugins: [
+// //     new webpack.ProvidePlugin({
+// //         $: 'jquery',
+// //         jQuery: 'jquery'
+// //     })
+// // ]
+  
+//   watch: true
+// };
